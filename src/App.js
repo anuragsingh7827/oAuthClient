279f4bc82f3627f54366181f2a83c83fb6e3af38
DyotisianAnurag <anurag@dyotis.com>
2023-05-08 10:38:50 +0530

    oAuthClient implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { GOOGLE_AUTH_LINK, LOGOUT_LINK } from "./constants";
+import Cookies from "js-cookie";
+import jwt_decode from "jwt-decode";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [user, setUser] = useState(null);
+
+  const fetchUser = () => {
+    const token = Cookies.get("x-auth-cookie");
+    if (token) {
+      Cookies.remove("x-auth-cookie");
+      const decodedToken = jwt_decode(token);
+      setUser(decodedToken);
+    }
+  };
+
+  const handleLogout = (e) => {
+    fetch(LOGOUT_LINK)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message);
+        setUser(null);
+      });
+  };
+
+  useEffect(() => fetchUser(), []);
+
+  return user ? (
+    <button onClick={handleLogout}>Logout</button>
+  ) : (
+    <a href={GOOGLE_AUTH_LINK}>Sign in with google</a>
   );
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const GOOGLE_AUTH_LINK = `${process.env.REACT_APP_BASE_URL}/auth/google`;
+export const LOGOUT_LINK = `${process.env.REACT_APP_BASE_URL}/auth/logout`;
+
+
